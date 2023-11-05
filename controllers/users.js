5e458512567f493c6cce422c8974fa7c2d5ce9be
afiefamarpurnomo <afiefamar00@gmail.com>
2023-11-05 19:26:27 +0700

    #14 Menambahkan Data

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const User = require('../models/Users')
+const User = require('../models/User')
 
 module.exports = {
     index: async (req, res) => {
@@ -22,15 +22,19 @@ module.exports = {
       }
         
       },
-    store: (req, res) => {
-        users.push(req.body)
-        res.json({
+    store: async (req, res) => {
+      try {
+        const user = await User.create(req.body)
+        res.status(200).json({
           status: true,
-          data: users,
+          data: user,
           method: req.method,
           url: req.url,
           message: "Data berhasil di tambahkan"
         })
+      } catch (error) {
+          res.status(400).json({sucess: false})
+      } 
       },
       update:  (req, res) => {
         const id = req.params.id
--- router/users.js
+++ router/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 
-const usercontrollers = require('../controllers/user')
+const usercontrollers = require('../controllers/users')
 
 routes.get('/users', usercontrollers.index)
 
